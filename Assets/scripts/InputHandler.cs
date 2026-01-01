a1363c452af32aefb5fb39cb0349bd975432f7c2
H00gy <dimitry.ross.hy@gmail.com>
2026-01-01 15:08:21 -0700

    imported player input and background
    Input function taken primarily from Youtube Tutorial credited to chonk
    link: https://www.youtube.com/watch?v=mRkFj8J7y_I&list=PL6yItMct2ybov1Z3InuFPpFmFY61NtOvH&index=42

---
+++ Assets/scripts/InputHandler.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    private Camera MainCamera; //camera view
+    private bool isDragging = false; // bool variable for checking when active
+    private Vector3 dragOffset;
+    private Transform draggedObject;
+
+    private void Awake()
+    {
+        SetCamera(Camera.main); //sets to where camera is
+    }
+
+
+
+    public void SetCamera(Camera cam)
+    {
+        MainCamera = cam;
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        if (context.started)
+        {
+            var rayHit = Physics2D.GetRayIntersection(MainCamera.ScreenPointToRay(mousePosition)); //looks for collider to hit and then drag
+            if (rayHit.collider)
+            {
+                Debug.Log("Click started on: " + rayHit.collider.gameObject.name);
+                isDragging = true;
+                draggedObject = rayHit.collider.transform;
+
+                Vector3 mouseWorldPos = MainCamera.ScreenToWorldPoint(mousePosition);
+                mouseWorldPos.z = 0f;
+                dragOffset = draggedObject.position - mouseWorldPos; // store offset
+            }
+        }
+        else if (context.canceled)
+        {
+            if (isDragging)
+            {
+                // Debug.Log("Drag ended");
+                isDragging = false;
+                draggedObject = null;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (isDragging && draggedObject != null)
+        {
+            Vector3 mouseWorldPos = MainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            mouseWorldPos.z = 0f;
+            draggedObject.position = mouseWorldPos + dragOffset; // maintain offset
+        }
+    }
+
+}
