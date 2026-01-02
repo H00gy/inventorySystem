d8d30a0e733b2754ad7a516728cfc6a26f884d80
H00gy <dimitry.ross.hy@gmail.com>
2026-01-02 16:47:02 -0700

    cam move and canvas changes

---
+++ Assets/scripts/camera/moveCam.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class moveCam : MonoBehaviour
+{
+    Camera cam;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    public void toInv()
+    {
+        cam.transform.position = new Vector3(0f, -3.62f, -8.337676f);
+    }
+    public void toMain()
+    {
+        cam.transform.position = new Vector3(0f, -3.62f, -8.337676f);
+    }
+
+    
+}
--- Assets/scripts/triggers/enterInv.cs
+++ Assets/scripts/triggers/enterInv.cs
@@ -2,17 +2,7 @@ using UnityEngine;
 
 public class enterInv : MonoBehaviour
 {
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        /*
-        Debug.Log("hit");
-        if (other.CompareTag("object"))
-        {
-            other.transform.position = new Vector2(Random.Range(-2.5f, 2.5f), Random.Range(-2.5f, -4.5f));
-            Debug.Log("objects new location is " + other.transform.position);
-        }
-        */
-    }
+    
     public void OnTriggerStay2D(Collider2D other)
     {
         Debug.Log("hit");
